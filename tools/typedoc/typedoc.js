195ef91e2d85dfb70251e9fcd0a5853e74cc5dce
胡松 <kevin.hs@antfin.com>
2021-02-22 14:28:54 +0800

    build: update doc generator

--- tools/typedoc/typedoc.js
+++ tools/typedoc/typedoc.js
@@ -1,21 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+
+fs.rmdirSync(path.join(process.cwd(), "api"), { recursive: true });
+
+const entryPoints = fs.readdirSync(path.join(process.cwd(), "packages")).map((pkg) => {
+  return `./packages/${pkg}/src/index.ts`;
+});
+
 module.exports = {
   name: "Oasis Engine",
-  mode: "modules",
+  // mode: "modules",
   out: `api/`,
-  theme: "./node_modules/@oasis-engine/typedoc-theme/bin/default",
-  ignoreCompilerErrors: true,
-  preserveConstEnums: true,
-  stripInternal: true,
-  excludeProtected: true,
-  "external-modulemap": "packages/([\\w\\-_]+)/",
-  exclude: [
-    "**/+(dev-packages|examples|typings)/**/*",
-    "**/*test.ts",
-    "packages/adapter-miniprogram/**/*",
-    "packages/component-miniprogram/**/*",
-    "packages/**/src/global.d.ts",
-    "packages/**/shaderLib/global.d.ts",
-    "scripts/**/*"
-  ],
-  plugin: ["@strictsoftware/typedoc-plugin-monorepo", 'typedoc-plugin-remove-references']
+  // theme: "./node_modules/@oasis-engine/typedoc-theme/bin/default",
+  entryPoints: entryPoints,
+  // ignoreCompilerErrors: true,
+  // preserveConstEnums: true,
+  // stripInternal: true,
+  // excludeProtected: true,
+  // "external-modulemap": "packages/([\\w\\-_]+)/",
+  // exclude: [
+  //   "**/+(dev-packages|examples|typings)/**/*",
+  //   "**/*test.ts",
+  //   "packages/adapter-miniprogram/**/*",
+  //   "packages/component-miniprogram/**/*",
+  //   "packages/**/src/global.d.ts",
+  //   "packages/**/shaderLib/global.d.ts",
+  //   "scripts/**/*"
+  // ],
+  // plugin: ["typedoc-plugin-remove-references"]
 };
