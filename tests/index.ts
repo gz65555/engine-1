e3294a41b52c141900d689999d7d0679f63414b4
gz65555 <gz65555@gmail.com>
2023-05-26 11:43:33 +0800

    test

--- tests/index.ts
+++ tests/index.ts
@@ -1,10 +1,15 @@
 import fs from "fs";
 import path from "path";
+import mocha from "mocha";
 
 function searchTests(root: string) {
   fs.readdirSync(root).forEach((file) => {
     const filePath = path.join(root, file);
     const stat = fs.statSync(filePath);
+    // @ts-ignore
+    mocha.setup({
+      timeout: 5000
+    });
     if (stat.isFile() && filePath.endsWith(".test.ts")) {
       require(filePath);
     } else if (stat.isDirectory()) {
